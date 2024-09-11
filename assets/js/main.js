faea69f2e38bfa0f465d7af98962890b22b84fad
Bulletshell <69229906+Bulletshell@users.noreply.github.com>
2024-09-11 17:42:52 +0700

    Add Contact & Email Section

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,7 +38,32 @@ const shadowHeader = () =>{
 window.addEventListener('scroll', shadowHeader);
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+    contactMessage = document.getElementById('contact-message')
 
+const sendEmail = (e) => {
+    e.preventDefault()
+
+    //serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_2e9tyvh','template_in30huo','#contact-form','OK-t0-0mxBZJnT8LG')
+    .then(()=>{
+        //Show sent message
+        contactMessage.textContent = 'Message Sent Successfully ✅'
+
+        //Remove message
+        setTimeout(()=>{
+            contactMessage.textContent = ''
+        }, 5000)
+
+        //Clear input fields
+        contactForm.reset()
+    }, ()=>{
+        //Show error message
+        contactMessage.textContent='Message Not Sent (Service Error) ❌'
+    })
+}
+
+contactForm.addEventListener('submit', sendEmail)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
